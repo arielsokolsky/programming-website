2d7d3c461b614f2b3afeaa39f39f8eba96ee593d
arielsokolsky <arielsoko123@gmail.com>
2021-07-22 15:49:44 +0300

    implemented signup page

--- website/src/pages/Signup.js
+++ website/src/pages/Signup.js
@@ -1,13 +1,34 @@
 import './Signup.css';
 import InputField from '../components/InputField';
 import { useState } from 'react';
-
+import Fetch from '../Fetch';
+import { wait } from '@testing-library/react';
+const SERVER_URL = "http://localhost:8080"
 
 function Signup()
 {
     const [name, setName] = useState("");
-    const [password, setPassword] = useState("")
-    const [validate, setValidate] = useState("")
+    const [password, setPassword] = useState("");
+    const [validate, setValidate] = useState("");
+    const [error, setError] = useState("");
+    
+    async function signup()
+    {
+        let response = await Fetch().post(SERVER_URL + "/signup", { name, password });
+        if (response.ok)
+        {
+            //to do: route to /login
+        }
+        else
+        {
+            setError(response.error ? response.error: "unknown error");
+        }
+    }
+
+    function login()
+    {
+        //to do: route to /login
+    }
 
     return (
         <div className="signupDiv">
@@ -15,24 +36,17 @@ function Signup()
             <InputField name="The name " placeholder="Enter name" setValue={setName} type="text" labelOnTop={true} labelClass = "signupLabel" inputClass ="signupInput"/>
             <InputField name="Password " placeholder="Enter password" setValue={setPassword} type="password" labelOnTop={true} labelClass = "signupLabel" inputClass ="signupInput"/>
             <InputField name="Password " placeholder="Enter password" setValue={setValidate} type="password" labelOnTop={true} labelClass = "signupLabel" inputClass ="signupInput"/>
-            <h1 className = "signupError">{(password == validate || validate=="") ? "" : "your password is invalid. please try again"}</h1>
+            <h1 className="signupError">{(password == validate || validate == "") ? "" : "your password is invalid. please try again"}</h1>
+            <h1 className="signupError">{error}</h1>
             <button onClick={signup} className="signupButton">Signup</button>
             <br />
             <button onClick={login} className = "signupButton">Login</button>
         </div>
 
     );
-}
 
-function signup()
-{
-   //to do: route to sign up
-}
 
-function login()
-{
-    //to do: route to menu
-}
 
+}
 
 export default Signup;
