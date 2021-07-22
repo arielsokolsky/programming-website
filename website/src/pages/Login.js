366d2f41f6eca8121a04df0f12a347c15b9a5377
Alon Regev <alonr3704@gmail.com>
2021-07-22 15:49:35 +0300

    implemented login logic

--- website/src/pages/Login.js
+++ website/src/pages/Login.js
@@ -3,10 +3,39 @@ import InputField from '../components/InputField.js';
 import { useState } from 'react';
 import Fetch from '../Fetch.js';
 
+const LOGIN_URL = "https://localhost:8080/login";
+
 function Login()
 {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+
+	// function routes to signup page
+	function signup()
+	{
+		// TODO: route to /signup
+		console.log("/signup");
+	}
+
+	// function attempts to log into the website
+	async function login()
+	{
+		let data = await Fetch.post(LOGIN_URL, { name: username, password: password });
+		
+		if (data.ok)
+		{
+			// TODO: route to /home
+			console.log("/home");
+		}
+		else
+		{
+			if (data.error)
+				setError(data.error);
+			else
+				setError("unknown error");
+		}
+	}
 
 	return (
 		<div className="loginDiv">
@@ -15,8 +44,10 @@ function Login()
 				setValue={setUsername} labelClass="loginLabel" inputClass="loginInput" />
 			<InputField name="password" placeholder="Enter password..." labelOnTop={true}
 				setValue={setPassword} type="password" labelClass="loginLabel" inputClass="loginInput" />
-			<button className="loginButton">Signup</button>
-			<button className="loginButton">Login</button>
+			<h3 className="loginError">{error}</h3>
+			
+			<button className="loginButton" onClick={signup}>Signup</button>
+			<button className="loginButton" onClick={login}>Login</button>
 		</div>
 	);
 }
