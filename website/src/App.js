7a07ad82a96917198827910cadcda862f817220e
Alon Regev <alonr3704@gmail.com>
2021-07-22 13:29:51 +0300

    code review changes

--- website/src/App.js
+++ website/src/App.js
@@ -10,12 +10,13 @@ function App()
 
 	function changeColorPalette()
 	{
-		setColorPalette((colorPalette + 1) % themePaths.length)
+		setColorPalette((colorPalette + 1) % themePaths.length);
 	}
 
 	return (
 		<div>
 			<style>
+				{/* import font Doppio One */}
 				@import url('https://fonts.googleapis.com/css2?family=Doppio+One&display=swap');
 			</style>
 			<link href={themePaths[colorPalette]} rel="stylesheet"/>
