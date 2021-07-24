9d0dcc0e0ad00c216822df0f9de085299c46c2b8
arielsokolsky <arielsoko123@gmail.com>
2021-07-24 13:48:08 +0300

    finish (check if exists) method

--- website/server/managers/MenuManager.js
+++ website/server/managers/MenuManager.js
@@ -8,98 +8,128 @@ class MenuManager
 	//add a post to the db
 	addPost(authorId, title, content)
 	{
-		this.database.run(`INSERT INTO Posts(author_id, title, content) VALUES (?, ?, ?);`,[authorId, title, content]);
+		this.database.run(`INSERT INTO Posts(author_id, title, content) VALUES (?, ?, ?);`, [authorId,title,content]).catch(console.log);
 	}
 
 	//add a commant
 	addComment(authorId, parentId, content)
 	{
-		this.database.run(`INSERT INTO Comments(author_id, object_id, content) VALUES(?, ?, ?);`, [authorId, parentId, content]);
+		this.database.run(`INSERT INTO Comments(author_id, object_id, content) VALUES(?, ?, ?);`,[authorId, parentId, content]).catch(console.log);
 	}
 
 	//add a subscribe to the user
 	subscribe(srdId, dstId)
 	{
-		this.database.run(`INSERT INTO Subscribers(src_user, dst_user) VALUES(?, ?);`, [srdId, dstId]);
+		this.database.run(`INSERT INTO Subscribers(src_user, dst_user) VALUES(?, ?);`,[srdId,dstId]).catch(console.log);
 	}
 
 	//add a like to a object
 	like(userId, objectId, value)
 	{
-		this.database.run(`INSERT INTO Likes(user_id, object_id, value) VALUES (?, ?, ?)`, [userId, objectId, value]);
+		this.database.run(`INSERT INTO Likes(user_id, object_id, value) VALUES (?, ?, ?)`,[userId,objectId,value]).catch(console.log);
 	}
 
 	//add likes to comment
 	addCommentLikes(commentId, amount)
 	{
-		this.database.run(`UPDATE Comments SET like_value = like_value + ? WHERE id = ? `, [amount, commentId]);
+		this.database.run(`UPDATE Comments SET like_value = like_value + ? WHERE id = ? `,[amount,commentId]).catch(console.log);
 	}
 
 	//add likes to post
 	addPostLikes(postId, amount)
 	{
-		this.database.run(
-      `UPDATE Posts SET like_value = like_value + ? WHERE id = ?`,
-      [amount, postId]
-    );
+		this.database.run(`UPDATE Posts SET like_value = like_value + ? WHERE id = ?`, [amount,postId]).catch(console.log);
 	}
 
 	//add subscribers
 	addSubscribers(userId, amount)
 	{
-		this.database.run(
-      `UPDATE Users SET subscriber_count = subscriber_count + ? WHERE id = ?`,
-      [amount, userId]
-    );
+		this.database.run(`UPDATE Users SET subscriber_count = subscriber_count + ? WHERE id = ?`,[amount, userId]).catch(console.log);
 	}
 
 	//remove a post from the db
 	removePost(postId)
 	{
-		this.database.run(`DELETE FROM Posts WHERE id = ?`, [postId]);
+		this.database.run(`DELETE FROM Posts WHERE id = ?`, [postId]).catch(console.log);
 	}
 
 	//remove a comment
 	removeComment(commentId)
 	{
-		this.database.run(`DELETE FROM Comments WHERE id = ?`, [commentId]);
+		this.database.run(`DELETE FROM Comments WHERE id = ?`, [commentId]).catch(console.log);
 	}
 
 	//get like values
-	async getLikeValue(userId, objectId) { }
+	async getLikeValue(userId, objectId)
+	{
+		return await this.database.get('SELECT value FROM Users WHERE user_id = ? AND object_id = ? ', [userId, objectId]);
+	}
 
 	//get all the info about the user
-	async getUserInfo(userId) { }
-
-	//get all the posts of the user
-	async getUserPosts(userId, previousIDs, sortBy) { }
-
-	//get all the posts
-	async getGlobalPosts(previousIDs, sortBy) { }
-
-	//get all the comments of object and filter it
-	async getComments(parentId, previousIDs, sortBy) { }
+	async getUserInfo(userId)
+	{
+		return await this.database.select('SELECT points, subscriber_count FROM Users WHERE id = ?', [userId]);
+	}
 
 	//get user by the post
-	async getPostOwner(postId) { }
+	async getPostOwner(postId)
+	{
+		return await this.database.get('SELECT author_id FROM Posts WHERE id = ?', [postId]);
+	}
 
 	//get user by the comment
-	async getCommentOwner(commentId) { }
+	async getCommentOwner(commentId)
+	{
+		return await this.database.get('SELECT author_id FROM Comments WHERE id = ?', [commentId]);
+	}
 
 	//check if object exists
-	async objectIdExists(id) { }
+	async objectIdExists(id)
+	{
+		return (await this.database.get('SELECT id FROM ObjectId WHERE id = ?', [id])) == id;
+	}
 
 	//check if post exist
-	async postExists(id) { }
+	async postExists(id)
+	{
+		return (await this.database.get("SELECT id FROM Posts WHERE id = ?", [id])) == id;
+	}
 
 	//check if comment exist
-	async commentExists(id) { }
+	async commentExists(id)
+	{
+		return (await this.database.get("SELECT id FROM Comments WHERE id = ?", [id])) == id;
+	}
 
 	//check if subscribed
-	async isSubscribed(srcId, destId) { }
+	async isSubscribed(srcId, dstId)
+	{
+		return (await this.database.get("SELECT src_user FROM Posts WHERE src_user = ? AND dst_user = ?", [srcId, dstId])) == srcId;
+	}
 
 	//get the type of the object
-	async objectType(id) { }
+	async objectType(id)
+	{
+		
+	}
+
+		//get all the posts of the user
+	async getUserPosts(userId, previousIDs, sortBy)
+	{
+		
+	}
+
+	//get all the posts
+	async getGlobalPosts(previousIDs, sortBy)
+	{
+		
+	}
+
+		//get all the comments of object and filter it
+	async getComments(parentId, previousIDs, sortBy)
+	{
+		
+	}
 }
 
 module.exports = MenuManager;
