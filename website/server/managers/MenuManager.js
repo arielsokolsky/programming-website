1f8a74eb1f64dcc11f06117131522451c40dd08d
arielsokolsky <arielsoko123@gmail.com>
2021-07-24 11:20:05 +0300

    initialize menu manager

--- website/server/managers/MenuManager.js
+++ website/server/managers/MenuManager.js
@@ -1,50 +1,68 @@
+class MenuManager {
+  constructor(database) {
+    this.database = database;
+  }
 
+  //add a post to the db
+  async addPost(authorId, title, content) {}
 
+  //remove a post from the db
+  async removePost(postId) {}
 
-class MenuManager
-{
-    constructor(database)
-    {
-        this.database = database;
-        
+  //add a commant
+  async addComment(authorId, parentId, content) {}
 
-    }
+  //remove a comment
+  async removeComment() {}
 
-    //add a post to the db 
-    addPost(authorId, title, content)
-    {
-        this.database.run(`INSERT INTO Posts(author_id, title, content) VALUES (?, ?, ?);`, [authorId, title, content]);
-    }
+  //add a subscribe to the user
+  async subscribe(srdId, dstId) {}
 
-    //remove a post from the db
-    removePost(postId)
-    {
+  //add a like to a object
+  async like(userId, objectId, value) {}
 
-    }
+  //get like values
+  async getLikeValue(userId, objectId) {}
 
-    //add a commant
-    addComment(authorId, parentId, content)
-    {
+  //check if subscribed
+  async isSubscribed(srcId, dstId) {}
 
-    }
+  //add likes to comment
+  async addCommentLikes(commentId, amount) {}
 
-    //remove a comment
-    removeComment()
-    {
+  //add likes to post
+  async addPostLikes(postId, amount) {}
 
-    }
+  //add subscribers
+  async addSubscribers(userId, amount) {}
 
-    //add a subscribe to the user
-    subscribe(srdId, dstId)
-    {
+  //get all the info about the user
+  async getUserInfo(userId) {}
 
-    }
+  //get all the posts of the user
+  async getUserPosts(userId, previousIDs, sortBy) {}
 
-    //add a like to a object
-    like(userId, objectId, value)
-    {
+  //get all the posts
+  async getGlobalPosts(previousIDs, sortBy) {}
 
-    }
+  //get all the comments of object and filter it
+  async getComments(parentId, previousIDs, sortBy) {}
+
+  //get user by the post
+  async getPostOwner(postId) {}
+
+  //get user by the comment
+  async getCommentOwner(commentId) {}
+
+  //check if object exists
+  async objectIdExists(id) {}
+
+  //check if post exist
+  async postExists(id) {}
+
+  //check if comment exist
+  async commentExists(id) {}
+
+  //get the type of the object
+  async objectType(id) {}
 }
-
-
